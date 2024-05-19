3a497db3365c596995f87042d7772423c7d0ec71
sammy-code98 <sammysmart98@yahoo.com>
2024-05-19 05:27:38 +0100

    feat:dynamic route

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Link } from 'react-router-dom';
 import { MdArrowBackIos } from "react-icons/md";
 import { AppRoutes } from '../../types/routes';
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,7 +17,7 @@ export default function Index(): JSX.Element {
       <div className="py-12 md:py-16 grid grid-cols-1 md:grid-cols-5 gap-6 md:gap-12 justify-items-center">
         {isLoading ? "Loading..." : (
           <>
-            {data?.map((country: { name: { common: string }; population: number; region: string; capital: string; flags: { png: string; alt: string } }) => (
+            {data?.map((country: { name: { common: string }; population: number; region: string; capital: string; flags: { png: string; alt: string }; cca3: string; }) => (
               <CountryCard key={country.name.common}
                 name={shortenString(country.name.common) || '____'}
                 population={NumComma(country.population) || 0}
@@ -25,6 +25,7 @@ export default function Index(): JSX.Element {
                 capital={country.capital || '____'}
                 img={country.flags.png}
                 alt={country.flags.alt || `${country.name.common}'s flag`}
+                code={country.cca3}
               />
             ))}
           </>
