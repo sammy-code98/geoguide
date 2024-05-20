bf07ef20328ddf36cc411bc47ad289bdf9827b9f
sammy-code98 <sammysmart98@yahoo.com>
2024-05-20 16:22:36 +0100

    fix:country details page

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,22 +1,31 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Key } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import { MdArrowBackIos } from "react-icons/md";
 import { getSpecificCountry } from '../../api/index.api';
 import { AppRoutes } from '../../types/routes';
+import { NumComma } from '../../utils/custom';
+
+
 export default function DetailsPage() {
   const { code } = useParams()
 
   const { isLoading, data } = useQuery({
     queryKey: ['getACountry'], queryFn: async () => {
-      const data = await getSpecificCountry(code as string)
-      console.log({ data });
+      const fetchedData = await getSpecificCountry(code as string)
+      console.log({ fetchedData });
 
-      return data
+      return fetchedData
     }
 
   })
   console.log(data, "jeje")
   if (isLoading) return <div>Loading...</div>
+
+  if (Array.isArray(data) && data.length === 1) {
+    const country = data[0]
+    const currency = Object.values(country?.currencies)
 
   return (
     <div className='px-4 sm:px-12 py-8'>
@@ -29,71 +38,76 @@ export default function DetailsPage() {
         </Link>
       </div>
 
+
       <div className='py-4 md:py-12'>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-40 items-center '>
           <div>
-            <img className="rounded-t-lg" src="https://nextui.org/images/hero-card-complete.jpeg" />
+            <img className="rounded-t-lg" src={country?.flags.png} />
           </div>
 
           <div>
-            <h5 className="mb-2 text-3xl font-bold tracking-light text-black">Counrty</h5>
+            <h5 className="mb-2 text-3xl font-bold tracking-light text-black">{country?.name.common}</h5>
 
             <div className='py-2 md:py-4 flex justify-between flex-wrap'>
               <div className='space-y-4'>
-                <p className="font-normal text-black">Native Name :
-                  <span className="text-primary ml-1">loeleoe</span>
-                </p>
                 <p className="font-normal text-black">Official Name :
-                  <span className="text-primary ml-1">loeleoe</span>
+                  <span className="text-primary ml-1">{country?.name.official}</span>
                 </p>
                 <p className="font-normal text-black">Population :
-                  <span className="text-primary ml-1">loeleoe</span>
+                  <span className="text-primary ml-1">{NumComma(country?.population)}</span>
                 </p>
                 <p className="font-normal text-black">Region :
-                  <span className="text-primary ml-1">loeleoe</span>
+                  <span className="text-primary ml-1">{country?.region}</span>
                 </p>
                 <p className="font-normal text-black">Sub Region :
-                  <span className="text-primary ml-1">loeleoe</span>
+                  <span className="text-primary ml-1">{country?.subregion}</span>
                 </p>
                 <p className="font-normal text-black">Capital :
-                  <span className="text-primary ml-1">loeleoe</span>
+                  <span className="text-primary ml-1">{country?.capital}</span>
                 </p>
-                <p className="font-normal text-black">Phone Code :
-                  <span className="text-primary ml-1">loeleoe</span>
+                <p className="font-normal text-black">Phone Code (IDD) :
+                  <span className="text-primary ml-1">{country?.idd?.root}</span>
                 </p>
                 <p className="font-normal text-black">Phone Suffixes :
-                  <span className="text-primary ml-1">loeleoe</span>
+                  {country?.idd?.suffixes.map((_: string | number, i: Key | null | undefined) => (
+                    <span key={i} className="text-primary ml-1">{_},</span>
+                  ))}
                 </p>
               </div>
 
               <div className='space-y-4'>
                 <p className="font-normal text-black">Top Level Domain :
-                  <span className="text-primary ml-1">loeleoe</span>
+                  <span className="text-primary ml-1">{country?.tld}</span>
                 </p>
                 <p className="font-normal text-black">Currency Name :
-                  <span className="text-primary ml-1">loeleoe</span>
+                  {currency.map((curr: any) => (
+                    <span key={curr.name} className="text-primary ml-1">{curr.name}</span>
+                  ))}
+
                 </p>
                 <p className="font-normal text-black">Currency Symbol :
-                  <span className="text-primary ml-1">loeleoe</span>
+                  {currency.map((curr: any) => (
+                    <span key={curr.symbol} className="text-primary ml-1">{curr.symbol}</span>
+                  ))}
                 </p>
                 <p className="font-normal text-black">Langauges :
                   <span className="text-primary ml-1">loeleoe</span>
                 </p>
                 <p className="font-normal text-black">UN Member :
-                  <span className="text-primary ml-1">loeleoe</span>
+                  {country?.unMember ? (<span className="text-primary ml-1">Yes</span>) : (<span className="text-primary ml-1">No</span>)}
                 </p>
                 <p className="font-normal text-black">Start of Week :
-                  <span className="text-primary ml-1">loeleoe</span>
+                  <span className="text-primary ml-1 capitalize">{country?.startOfWeek}</span>
                 </p>
                 <p className="font-normal text-black">Time Zone :
-                  <span className="text-primary ml-1">loeleoe</span>
+                  <span className="text-primary ml-1">{country?.timezones}</span>
                 </p>
               </div>
             </div>
 
             <div className='py-4'>
               <p className="font-normal text-black">Border Countries :
-                {[1, 2, 3, 4].map((_, i) => (
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((_, i) => (
                   <span key={i} className='py-1 px-4 text-primary border border-grayLight ml-2 rounded-md text-sm text-center cursor-pointer'>country</span>
                 ))}
               </p>
@@ -103,4 +117,5 @@ export default function DetailsPage() {
       </div>
     </div>
   )
+  }
 }
