4ecaa5d4096653349c4238f3e679d0159380d38e
sammy-code98 <sammysmart98@yahoo.com>
2024-06-08 16:17:52 +0100

    add error boundary page

---
+++ src/modules/ErrorPage.tsx
@@ -0,0 +1,36 @@
+import { useRouteError, NavLink } from "react-router-dom"
+
+
+export default function ErrorPage():JSX.Element {
+    const error = useRouteError()
+    console.log(error);
+    
+  return (
+    <div className="h-screen flex flex-col justify-center items-center  text-center">
+        <h1 className="mb-4 text-8xl font-semibold text-primary">404</h1>
+        <p className="mb-4 text-xl text-textGray">Oops! Looks like you're lost.</p>
+        <div className="animate-bounce">
+        <svg
+          className="mx-auto h-16 w-16 text-primary"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+          ></path>
+        </svg>
+        </div>
+        <p className="mt-4 text-textGray">
+        Let's get you back {" "}
+        <NavLink to="/" className="text-primary font-semibold">
+          home
+        </NavLink>
+        .
+        </p>
+    </div>
+  )
+}
